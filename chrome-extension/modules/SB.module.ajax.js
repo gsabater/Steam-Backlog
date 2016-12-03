afe9d9662c82b6ebb5f577f09327c7531a85fb5c
Gaspar Sabater <gaspar.sabater@bonda.es>
2016-12-03 03:31:05 +0100

    more achievements fix

--- chrome-extension/modules/SB.module.ajax.js
+++ chrome-extension/modules/SB.module.ajax.js
@@ -120,7 +120,7 @@
       db[gameID].updated = n;
 
       // Avoid one ajax call if no achievements
-      if((data.achievements === false) || (data.achievements === 0)){
+      if(db[gameID].wishlist || !data.achievements || (data.achievements === false) || (data.achievements === 0)){
         howLongToBeatSteam(gameID);
         saveGameInfo(gameID);
         return;
