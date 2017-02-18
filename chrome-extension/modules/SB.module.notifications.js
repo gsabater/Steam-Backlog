5bb4fb7179ee8279408e1855fd8bfc35064a7955
Gaspar Sabater <gaspar.sabater@bonda.es>
2017-02-18 02:29:26 +0100

    Background notifications for extension updates, last sync timestamp

--- chrome-extension/modules/SB.module.notifications.js
+++ chrome-extension/modules/SB.module.notifications.js
@@ -1,87 +1,99 @@
-  //+-------------------------------------------------------
-  //| sendPush()
-  //| + Sends a notification to the browser with details
-  //+-------------------------------------------------------
-    function sendPush(notID, text, badge, subtext){
+//=================================================================
+//
+//  ██████╗  █████╗  ██████╗██╗  ██╗██╗      ██████╗  ██████╗
+//  ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║     ██╔═══██╗██╔════╝
+//  ██████╔╝███████║██║     █████╔╝ ██║     ██║   ██║██║  ███╗
+//  ██╔══██╗██╔══██║██║     ██╔═██╗ ██║     ██║   ██║██║   ██║
+//  ██████╔╝██║  ██║╚██████╗██║  ██╗███████╗╚██████╔╝╚██████╔╝
+//  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝
+//
+//=================================================================
 
-      if(!badge){ _num++; }
+//+-------------------------------------------------------
+//| sendPush()
+//| + Sends a notification to the browser with details
+//+-------------------------------------------------------
+function sendPush(notID, message, contextMessage)
+{
 
-      var options = {
+    var options = {
         type: "basic",
-        iconUrl: "/assets/icon38.png",
+        iconUrl: "/assets/img/icon38.png",
 
-        title: "Mediavida Notifier",
-        message: text,
-        contextMessage: (subtext)? (subtext.charAt(0).toUpperCase() + subtext.slice(1)) : "",
+        title: "Steam Backlog",
+        message: message,
+        contextMessage: (contextMessage)? (contextMessage.charAt(0).toUpperCase() + contextMessage.slice(1)) : "",
 
         priority: 2,
         /*
         buttons: [{
-            title: "Yes, get me there",
-            iconUrl: "/assets/MVN_128x128.png"
+                title: "Yes, get me there",
+                iconUrl: "/assets/MVN_128x128.png"
         }, {
-            title: "Get out of my way",
-            iconUrl: "/assets/MVN_128x128.png"
+                title: "Get out of my way",
+                iconUrl: "/assets/MVN_128x128.png"
         }]
         */
-      }
-
-      chrome.notifications.create(notID, options);
-
-      window.setTimeout(function(){ updatePush(notID); }, 4000);
-      window.setTimeout(function(){ clearNotification(notID); }, 10000);
     }
+
+    chrome.notifications.create(notID, options);
+    window.setTimeout(function(){ clearNotification(notID); }, 10000);
+}
+
+
+//+-------------------------------------------------------
+//| clearNotification()
+//| + Removes the notification from the action center and
+//| + fades the audio background.
+//+-------------------------------------------------------
+function clearNotification(notificationId)
+{
+    chrome.notifications.clear(notificationId);
+}
+
+
 /*
-  //+-------------------------------------------------------
-  //| updatePush()
-  //| + Updates the options so the update lasts longer
-  //+-------------------------------------------------------
-    function updatePush(notID){
-      var options = { priority: 1, }
-      chrome.notifications.update(notID, options);
-    }
+
+    //+-------------------------------------------------------
+    //| pushAction()
+    //| + Opens link and deletes notification from center
+    //+-------------------------------------------------------
+        function pushAction(notificationId){
+
+            clearNotification(notificationId);
+
+            var base = chrome.extension.getURL("");
+            var url = "http://www.mediavida.com/"+notifications[notificationId].url.split(base)[1];
+            chrome.tabs.create({"url":url,"selected":true});
+        }
 
 
-  //+-------------------------------------------------------
-  //| pushAction()
-  //| + Opens link and deletes notification from center
-  //+-------------------------------------------------------
-    function pushAction(notificationId){
-
-      clearNotification(notificationId);
-
-      var base = chrome.extension.getURL("");
-      var url = "http://www.mediavida.com/"+notifications[notificationId].url.split(base)[1];
-      chrome.tabs.create({"url":url,"selected":true});
-    }
 
 
-  //+-------------------------------------------------------
-  //| clearNotification()
-  //| + Removes the notification from the action center and 
-  //| + fades the audio background.
-  //+-------------------------------------------------------
-    function clearNotification(notificationId){
-      chrome.notifications.clear(notificationId);
-    }
 
+    //| + Multiple event listeners for chrome runtime
+    //+-------------------------------------------------------
+        chrome.notifications.onClicked.addListener(pushAction);
+        chrome.notifications.onClosed.addListener(clearNotification);
 
-  //| + Multiple event listeners for chrome runtime
-  //+-------------------------------------------------------
-    chrome.notifications.onClicked.addListener(pushAction);
-    chrome.notifications.onClosed.addListener(clearNotification);
-    
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-      console.log("+ MVN: Message event", request);
+        chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+            console.log("+ MVN: Message event", request);
 
-      if (request.options)            { setMVNStorage(request.options, true); }
-      if (request.mvnLS)              { setStorage(request.mvnLS); }
+            if (request.options)            { setMVNStorage(request.options, true); }
+            if (request.mvnLS)              { setStorage(request.mvnLS); }
 
-      if (request.clear == "0")       { _num = 0; chrome.browserAction.setBadgeText({text:""}); }
-      if (request.test == "true")     { sendPush(Math.floor((Math.random() * 100) + 1).toString(), "Prueba de notificación", true, "Ahora mismo"); }
-      if (request.mvnBadge == "num")  { sendResponse({farewell: _num}); }
+            if (request.clear == "0")       { _num = 0; chrome.browserAction.setBadgeText({text:""}); }
+            if (request.test == "true")     { sendPush(Math.floor((Math.random() * 100) + 1).toString(), "Prueba de notificación", true, "Ahora mismo"); }
+            if (request.mvnBadge == "num")  { sendResponse({farewell: _num}); }
 
-      if (request.getUser == "object"){ sendResponse({farewell: user}); }
-      if (request.getUser == "full")  { sendResponse({user: user, ls: mvnLS}); }
-    });
+            if (request.getUser == "object"){ sendResponse({farewell: user}); }
+            if (request.getUser == "full")  { sendResponse({user: user, ls: mvnLS}); }
+        });
 */
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+    console.log("Message event", request);
+
+    if (request.push){ sendPush(request.push.notID, request.push.message, request.push.contextMessage); }
+});
--- chrome-extension/sb.main.js
+++ chrome-extension/sb.main.js
@@ -9,15 +9,15 @@
 //
 //=================================================================
 
-var v = "0.9.9";
+var v = "1.0";
 console.log("%c Steam Backlog v" + v + " ", 'background: #222; color: #bada55');
 
 var isAngular    = false,       // flag used to know if the execution is done in the angular panel
     isOwnProfile = false,       // flag used to know if window.location is own profile steam page
 
-    user         = false,            // chrome.local var
-    db           = false,            // chrome.local var
-    hltbs        = false,            // howlongtobeatsteam var
+    user         = false,       // chrome.local var
+    db           = false,       // chrome.local var
+    hltbs        = false,       // howlongtobeatsteam var
     savedb       = false,
 
     queue        = [],          // used to store pending apps waiting to be updated
@@ -27,21 +27,21 @@ var isAngular    = false,       // flag used to know if the execution is done in
 
     collections  = false,        // chrome.local var
     settings     = {
-      v: v,
-      scan: {
-        interval: "3",
-        achievements: false
-      },
-      library:{
-        wishlist: false
-      }
+        v: v,
+        scan: {
+            interval: "3",
+            achievements: false
+        },
+        library:{
+            wishlist: false
+        }
     };
 
 
-  // Initialize app only when needed
-  window.setTimeout(function(){
+// Initialize app only when needed
+window.setTimeout(function(){
     init();
-  }, 50);
+}, 50);
 
 
 //+-------------------------------------------------------
@@ -50,9 +50,10 @@ var isAngular    = false,       // flag used to know if the execution is done in
 //| + gets chrome.local information when needed
 //| + Adds layout modifications
 //+-------------------------------------------------------
-  function init(){
+function init()
+{
 
-    // Flag if angular app (dashboard / backlog)
+    // Flag when inside the angular app
     var windowURL = window.location.pathname.replace(/^\/|\/$/g, '').toLowerCase();
     isAngular = (windowURL == "steam-backlog.html")? true : false;
 
@@ -62,40 +63,50 @@ var isAngular    = false,       // flag used to know if the execution is done in
     // Add backlog menu option
     $('<a class="menuitem" href="'+chrome.extension.getURL("/steam-backlog.html")+'">BACKLOG</a>').insertAfter(".menuitem.supernav.username");
 
-    // Initialize localstorage
+    // Initialize Chrome Storage
     initChromeLocal();
 
-    /* // Update notification
-    if(parseFloat(options.v) == parseFloat(v)){
-      console.warn(options, parseFloat(options.v) , parseFloat(v));
-      //sendPush("update", "Mediavida Notifier ha sido actualizada a la versión "+v, true);
-    }
-    */
+}
 
-  }
 
-  //+-------------------------------------------------------
-  //| init()
-  //+--------------------------------
-  //| + gets chrome.local information when needed
-  //| + Sets global db and user vars
-  //+-------------------------------------------------------
-    function initChromeLocal(storage){
+//+-------------------------------------------------------
+//| initChromeLocal()
+//+--------------------------------
+//| + gets chrome.local information when needed
+//| + Sets global db and user vars
+//+-------------------------------------------------------
+function initChromeLocal(storage)
+{
 
-      if(!storage){
+    if(!storage){
         chrome.storage.local.get(null, function(items){
-          console.warn("Steam Backlog stored vars", items); initChromeLocal(items); });
-          return;
-      }else{
+            console.warn("Steam Backlog stored vars", items); initChromeLocal(items); });
+            return;
+    }else{
 
         // Set global user and db vars
         db          = (storage.db)? storage.db : {};
         user        = (storage.user)? storage.user : {};
         settings    = (storage.settings)? storage.settings : settings;
         collections = (storage.collections)? storage.collections : false;
-      }
+    }
 
-     // Update user information
-     // -> module.user
-     GetPlayerSummaries();
- }
+    // Initialize user data update
+    // -> module.user
+    GetPlayerSummaries();
+
+    // Notify the user about a new extension update
+    if(parseFloat(v) > parseFloat(settings.v)){
+
+        var options = {
+            notID: "update-alert",
+            title: "Steam Backlog",
+            message: "Has been updated to version "+ v,
+            contextMessage: "Click here to see what's new",
+        };
+
+        chrome.runtime.sendMessage({push: options}, function(response){
+            //console.log("...");
+        });
+    }
+}
